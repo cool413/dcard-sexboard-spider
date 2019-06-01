bd4773f36836cf7d8048c08c4a9c385e6f00bcab
derekyou <cool82413@gmail.com>
2019-06-02 03:04:46 +0800

    try timer and ticker

--- main.go
+++ main.go
@@ -6,6 +6,8 @@ import (
 	"io/ioutil"
 	"log"
 	"net/http"
+	"strconv"
+	"time"
 )
 
 const (
@@ -14,79 +16,90 @@ const (
 	BoardURL   = "http://www.dcard.tw/f/sex/p/"
 )
 
-type Bird struct {
-	Species     string
-	Description string
-}
-
 type ArticleInfo struct {
-	ID        int     `json:"id"`
+	ID        int32   `json:"id"`
 	Title     string  `json:"title"`
 	Media     []Media `json:"media"`
 	MediaMeta []interface{}
 }
 
 type Media struct {
-	URL string
+	PicURL string `json:"url"`
 }
 
-// type MediaMeta struct {
-// 	ID            string `json:"id"`
-// 	URL           string `json:"url"`
-// 	type string `json:"type"`
-// }
-
 func main() {
+	var NowID int32 = 0
 
+	for {
+		log.Println(time.Now())
+		log.Println("NowId=", NowID)
+
+		SexArticles, err := getLatestList(NowID)
+		if err != nil {
+			log.Println(err)
+			return
+		}
+		//fmt.Printf("%#v\n", SexArticles)
+
+		for _, value := range SexArticles {
+			fmt.Println("id=", value.ID)
+			fmt.Printf("Title= %s \n", value.Title)
+			fmt.Println("-------Media-------")
+			for _, MediaValue := range value.Media {
+				fmt.Printf("PicURL= %s \n", MediaValue.PicURL)
+			}
+			fmt.Println("-------MediaMeta-------")
+			for _, MediaMetaValue := range value.MediaMeta {
+				fmt.Printf("id= %s \n", MediaMetaValue.(map[string]interface{})["id"].(string))
+				fmt.Printf("url= %s \n", MediaMetaValue.(map[string]interface{})["url"].(string))
+				fmt.Printf("type= %s \n\n", MediaMetaValue.(map[string]interface{})["type"].(string))
+			}
+			//fmt.Printf("MediaMeta=%v \n", value.MediaMeta)
+
+			if value.ID > NowID {
+				NowID = value.ID
+			}
+		}
+
+		time.Sleep(10 * time.Second)
+	}
+
+}
+
+func getLatestList(LatestID int32) ([]ArticleInfo, error) {
 	params := make(map[string]string)
-	params["limit"] = "1"
-
+	params["limit"] = "10"
+	if LatestID != 0 {
+		params["after"] = strconv.FormatInt(int64(LatestID), 10)
+	}
 	resp, err := Get(ListURL, params, nil)
 	if err != nil {
 		log.Println(err)
-		return
+		return nil, err
 	}
 
 	defer resp.Body.Close()
-
 	sitemap, err := ioutil.ReadAll(resp.Body)
 	if err != nil {
 		log.Println(err)
-		return
+		return nil, err
 	}
 
-	fmt.Println(string(sitemap))
-	var SexArticles []ArticleInfo
-	if err := json.Unmarshal([]byte(sitemap), &SexArticles); err != nil {
-		fmt.Println("error:", err)
+	//fmt.Println(string(sitemap))
+
+	var Articles []ArticleInfo
+	if err := json.Unmarshal([]byte(sitemap), &Articles); err != nil {
+		log.Println(string(sitemap))
+		return nil, fmt.Errorf("json.Unmarshal error:%s", err)
 	}
 
-	//fmt.Printf("%#v\n", SexArticles)
-
-	for _, value := range SexArticles {
-		fmt.Println("id=", value.ID)
-		fmt.Printf("Title= %s \n", value.Title)
-		fmt.Println("-------Media-------")
-		for _, MediaValue := range value.Media {
-			fmt.Printf("URL= %v \n", MediaValue)
-		}
-		fmt.Println("-------MediaMeta-------")
-		for _, MediaMetaValue := range value.MediaMeta {
-			fmt.Printf("id= %s \n", MediaMetaValue.(map[string]interface{})["id"].(string))
-			fmt.Printf("url= %s \n", MediaMetaValue.(map[string]interface{})["url"].(string))
-			fmt.Printf("type= %s \n\n", MediaMetaValue.(map[string]interface{})["type"].(string))
-		}
-		//fmt.Printf("MediaMeta=%v \n", value.MediaMeta)
-	}
-
-	//fmt.Println(SexArticles[0].MediaMeta[0].(map[string]interface{})["id"].(string))
-	//fmt.Println(SexArticles.MediaMeta.(map[string]interface{})["normalizedUrl"].(string))
-
+	return Articles, nil
 }
 
 //Get http get method
 func Get(url string, params map[string]string, headers map[string]string) (*http.Response, error) {
 	req, err := http.NewRequest("GET", url, nil)
+
 	if err != nil {
 		log.Println(err)
 		//nil只能賦值給指標
